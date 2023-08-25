6c22bd866f481baba02987391cf5ecab94f30dad
Kanmii <maneptha@gmail.com>
2023-08-25 13:15:36 -0400

    normalize-title.js to include save directory path

--- 0000-normalize-title.js
+++ 0000-normalize-title.js
@@ -9,11 +9,23 @@ pattern = /[|,\s()-]+/g;
 
 replace_with_empty_text_pattern = /[']+|(?:_-)/g;
 
-let outText = inText
+const title = inText
   .toLowerCase()
   .trim()
   .replace(pattern, "-")
   .replace(replace_with_empty_text_pattern, "")
   .replace(/-?\.htm/, ".htm");
+
+const outText = [
+  `C:\\google-drive-maneptha\\web-pages\\${title}`,
+  "\n",
+  `/c/google-drive-maneptha/web-pages/${title}`,
+  "\n",
+  "C:\\google-drive-maneptha\\web-pages",
+  "\n",
+  "/c/google-drive-maneptha/web-pages",
+  "\n",
+  title,
+].join("\n");
 
 fs.writeFileSync(outFile, outText);
