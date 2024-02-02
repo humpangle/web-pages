1dabd0d1e85f69bc7df33c888044d18501af9aab
Kanmii <maneptha@gmail.com>
2024-02-02 13:15:05 -0500

    normalize-title.js pattern updated

--- 0000-normalize-title.js
+++ 0000-normalize-title.js
@@ -7,7 +7,7 @@ const outFile = ".___scratch.out.txt";
 
 let inText = fs.readFileSync(inFile, { encoding: "utf8" });
 
-pattern = /[⧸\\/\[\]|｜,\s()-]+/g;
+pattern = /[`⧸\\/\[\]|｜,\s()-]+/g;
 
 replace_with_empty_text_pattern = /[']+|(?:_-)/g;
 
